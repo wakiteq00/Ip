591e96e44d1b5ac5bdc48cf20421b67a9f05751a
wakiteq00 <81784972+wakiteq00@users.noreply.github.com>
2025-07-01 15:06:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.getElementById("verifyBtn").addEventListener("click", async () => {
     } else if (text.includes("verified") || res.ok) {
       showMessage("✅ Verified! Back to Bot");
       setTimeout(() => {
-        window.location.href = "https://t.me/PayAIRTIMECARDBOT?start=VERIFIED";
+        window.location.href = "https://t.me/tPayAIRTIMECARDBOT?start=VERIFIED";
       }, 1500);
     } else {
       showMessage("❌ Unknown error occurred.\n\n" + text);
